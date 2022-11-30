d362d6644df0d2f7c23b635941cd62745dd09688
SlWa99 <walid.slimani99@gmail.com>
2022-11-30 16:49:01 +0100

    test

--- WSI_OBE_POO2022-L8-Chess/src/engine/ChessController.java
+++ WSI_OBE_POO2022-L8-Chess/src/engine/ChessController.java
@@ -9,7 +9,7 @@ public class ChessController implements chess.ChessController {
     public void start(ChessView view) {
         view.startView();
 
-        view.putPiece(PieceType.ROOK, PlayerColor.WHITE, 0, 1);
+        view.putPiece(PieceType.ROOK, PlayerColor.WHITE, 1, 1);
         view.putPiece(PieceType.ROOK, PlayerColor.WHITE, 2, 1);
 
     }
