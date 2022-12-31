1835ba31d02187f091321e593e60c2cca42c7b93
SlWa99 <walid.slimani99@gmail.com>
2023-01-01 00:25:20 +0100

    check and checkmate

--- WSI_OBE_POO2022-L8-Chess/src/engine/ChessController.java
+++ WSI_OBE_POO2022-L8-Chess/src/engine/ChessController.java
@@ -6,8 +6,10 @@ import chess.PlayerColor;
 import engine.util.Coord;
 import engine.util.PromotionChoice;
 
+import java.lang.reflect.Type;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class ChessController implements chess.ChessController {
     private final int SIZE = 8;
@@ -16,40 +18,66 @@ public class ChessController implements chess.ChessController {
     private ChessView view;
     private PlayerColor turn;
     private Piece[][] board;
+    //private boolean check;
+    private boolean checkMate;
+
 
     @Override
     public void start(ChessView view) {
         this.view = view;
         view.startView();
         board = new Piece[SIZE][SIZE];
+        checkMate = false;
         newGame();
     }
 
     @Override
     public boolean move(int fromX, int fromY, int toX, int toY) {
-        //TODO
-        // Verif echec et/ou mat
-        // Si en echec forcé la protection
+        if (checkMate) {
+            view.displayMessage("Game finished, please start again");
+            return false;
+        }
 
         // si il n'y a pas de piece sur cette case. on ne pourra pas se déplacer
-        if(!acceptMove(fromX,fromY,toX,toY)) {return false;}
-        List<List<Coord>> moves = refactorListMove(getMoves(fromX,fromY,toX,toY));
-        if(!findCoordInListMove(moves,toX,toY)) return false;
+        if (!acceptMove(fromX, fromY, toX, toY)) {
+            return false;
+        }
+        List<List<Coord>> moves = refactorListMove(getMoves(fromX, fromY, toX, toY));
+        if (!findCoordInListMove(moves, toX, toY)) return false;
         // si gestion castle return false ça veut dire qu'on fait pas un rook (pas de déplacement)
-        if(!gestionCastle(fromX,fromY,toX,toY)) {
+        if (!gestionCastle(fromX, fromY, toX, toY)) {
             gestionEnPassant(fromX, fromY, toX, toY);
             movePiece(fromX, fromY, toX, toY);
             gestionPromotion(fromX, fromY, toX, toY);
         }
+
+        // On vérifie qu'on ne se mette pas en échec tout seul
+        if (checkSelfMat()) {
+            view.displayMessage("Prohibited movement !");
+            movePiece(toX, toY, fromX, fromY);
+            return false;
+        }
+
+        // On regarde s'il y a échec et remplie une liste de toutes les pièces qui mettent échec
+        LinkedList<Coord> attackers = new LinkedList<>();
+        if (checkMat(attackers)) {
+            view.displayMessage("The " + (turn == PlayerColor.WHITE ? "BLACK" : "WHITE") + " king is in check !");
+            if (!canProtectHisKingByEating(attackers) && !canCoverHisKing(attackers) && !canMoveHisKing(attackers)) {
+                checkMate = true;
+                view.displayMessage("Checkmate, " + (turn == PlayerColor.WHITE ? "BLACK" : "WHITE") + " wins !");
+                return true;
+            }
+        }
+
         changeTurn();
         return true;
     }
 
-    public boolean findCoordInListMove(List<List<Coord>> listMove,int toX,int toY){
-        Coord find = new Coord(toX,toY);
-        for(List<Coord> list : listMove){
-            for(Coord c : list){
-                if(find.isEqual(c)){
+    public boolean findCoordInListMove(List<List<Coord>> listMove, int toX, int toY) {
+        Coord find = new Coord(toX, toY);
+        for (List<Coord> list : listMove) {
+            for (Coord c : list) {
+                if (find.isEqual(c)) {
                     return true;
                 }
             }
@@ -66,6 +94,11 @@ public class ChessController implements chess.ChessController {
                 if (board[c.getX()][c.getY()] != null) {
                     if (board[c.getX()][c.getY()].getColor() != turn) {
                         refactoredVect.add(c);
+                        if (vect.indexOf(c) < vect.size() && board[c.getX()][c.getY()].type != PieceType.KNIGHT) {
+                            break;
+                        }
+                    } else if (board[c.getX()][c.getY()].type == PieceType.KING) {
+                        refactoredVect.add(c);
                     } else {
                         break;
                     }
@@ -73,11 +106,14 @@ public class ChessController implements chess.ChessController {
                     refactoredVect.add(c);
                 }
             }
-            refactorListMove.add(refactoredVect);
+            if (!refactoredVect.isEmpty()) {
+                refactorListMove.add(refactoredVect);
+            }
         }
         return refactorListMove;
     }
-    public boolean acceptMove(int fromX, int fromY, int toX, int toY){
+
+    public boolean acceptMove(int fromX, int fromY, int toX, int toY) {
         if (board[fromX][fromY] == null) return false;
         // si on essaie de deplacer une pièce de la mauvaise couleur
         if (board[fromX][fromY].getColor() != turn) return false;
@@ -87,9 +123,10 @@ public class ChessController implements chess.ChessController {
         if (board[toX][toY] != null && board[toX][toY].getColor() == turn) return false;
         return true;
     }
-    public List<List<Coord>> getMoves(int fromX, int fromY, int toX, int toY){
-        if(board[toX][toY] == null){
-            if(board[fromX][fromY].getType() == PieceType.PAWN && toX == pawnJumpStart && fromY == (turn == PlayerColor.WHITE?4:3)){
+
+    public List<List<Coord>> getMoves(int fromX, int fromY, int toX, int toY) {
+        if (board[toX][toY] == null) {
+            if (board[fromX][fromY].getType() == PieceType.PAWN && toX == pawnJumpStart && fromY == (turn == PlayerColor.WHITE ? 4 : 3)) {
                 return board[fromX][fromY].listEatingMove();
             } else {
                 return board[fromX][fromY].listMove();
@@ -98,52 +135,57 @@ public class ChessController implements chess.ChessController {
             return board[fromX][fromY].listEatingMove();
         }
     }
-    public void gestionEnPassant(int fromX, int fromY, int toX, int toY){
+
+    public void gestionEnPassant(int fromX, int fromY, int toX, int toY) {
         // check enpassant
-        if(board[fromX][fromY].getType() == PieceType.PAWN && toX == pawnJumpStart && fromY == (turn == PlayerColor.WHITE?4:3)){
-            view.removePiece(pawnJumpStart,(turn == PlayerColor.WHITE?4:3));
+        if (board[fromX][fromY].getType() == PieceType.PAWN && toX == pawnJumpStart && fromY == (turn == PlayerColor.WHITE ? 4 : 3)) {
+            view.removePiece(pawnJumpStart, (turn == PlayerColor.WHITE ? 4 : 3));
         }
-        if(board[fromX][fromY].getType() == PieceType.PAWN && Math.abs(fromY - toY) == 2){
+        if (board[fromX][fromY].getType() == PieceType.PAWN && Math.abs(fromY - toY) == 2) {
             pawnJumpStart = fromX;
         } else {
             pawnJumpStart = -1;
         }
     }
-    public boolean gestionCastle(int fromX,int fromY,int toX, int toY){
+
+    public boolean gestionCastle(int fromX, int fromY, int toX, int toY) {
         // check si on a un roi et une tour de la même couleur et qui n'ont pas bougé
-        if(board[fromX][fromY].getType() != PieceType.KING
+        if (board[fromX][fromY].getType() != PieceType.KING
                 || board[toX][toY] != null
                 || board[fromX][fromY].hasMoved) return false;
-        int castleX = (toX == Math.min(fromX, toX) ? 0:7);
-        if(!(board[castleX][fromY] != null
+        int castleX = (toX == Math.min(fromX, toX) ? 0 : 7);
+        if (!(board[castleX][fromY] != null
                 && board[castleX][fromY].getType() == PieceType.ROOK
                 && board[castleX][fromY].getColor() == board[fromX][fromY].getColor()
                 && !board[castleX][fromY].hasMoved)) return false;
-        int left = Math.min(fromX,castleX);
+        int left = Math.min(fromX, castleX);
         int right = Math.max(fromX, castleX);
         // vérif si la voie est libre entre le roi et la tour
-        for(int i = left + 1; i < right; ++i){
-            if(board[i][fromY] != null) return false;
+        for (int i = left + 1; i < right; ++i) {
+            if (board[i][fromY] != null) return false;
         }
         // on peut effectuer le rock
         // bouge le roi
-        movePiece(fromX,fromY,toX,toY);
+        movePiece(fromX, fromY, toX, toY);
         // bouge la tour
-        movePiece(castleX,fromY,toX + ((fromX-toX)/2),toY);
+        movePiece(castleX, fromY, toX + ((fromX - toX) / 2), toY);
         return true;
     }
-    public void changeTurn(){
-        turn = (turn == PlayerColor.WHITE?PlayerColor.BLACK:PlayerColor.WHITE);
-        view.displayMessage("Turn : " + (turn== PlayerColor.WHITE ?"WHITE":"BLACK"));
+
+    public void changeTurn() {
+        turn = (turn == PlayerColor.WHITE ? PlayerColor.BLACK : PlayerColor.WHITE);
+        //view.displayMessage("Turn : " + (turn == PlayerColor.WHITE ? "WHITE" : "BLACK"));
     }
-    public void movePiece(int fromX, int fromY, int toX, int toY){
+
+    public void movePiece(int fromX, int fromY, int toX, int toY) {
         // déplacer la pièce au bon endroit
         board[fromX][fromY].move(toX, toY);
         board[toX][toY] = board[fromX][fromY];
         board[fromX][fromY] = null;
-        view.removePiece(fromX,fromY);
-        view.putPiece(board[toX][toY].getType(),board[toX][toY].getColor(),toX,toY);
+        view.removePiece(fromX, fromY);
+        view.putPiece(board[toX][toY].getType(), board[toX][toY].getColor(), toX, toY);
     }
+
     @Override
     public void newGame() {
         cleanGUI();
@@ -257,5 +299,236 @@ public class ChessController implements chess.ChessController {
         Piece p = result.create();
         board[toX][toY] = p;
         view.putPiece(p.getType(), p.getColor(), toX, toY);
+    }
+
+    private boolean checkMat(LinkedList<Coord> attackers) {
+        // On cherche la position du roi adverse
+        List<Coord> kingPos = findKings();
+        Coord enemyKingPos = turn == PlayerColor.WHITE ? kingPos.get(1) : kingPos.get(0);
+
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (board[i][j] != null && board[i][j].color == turn) {
+                    // On récupère la liste des positions ou on peut manger une pièce adverse
+                    List<List<Coord>> moves = board[i][j].listEatingMove();
+                    List<List<Coord>> eatingMoves = refactorListMove(moves);
+
+                    // On regarde si une pièce peut manger le roi adverse
+                    if (findCoordInListMove(eatingMoves, enemyKingPos.getX(), enemyKingPos.getY())) {
+                        attackers.add(board[i][j].coord);
+                    }
+                }
+            }
+        }
+        return attackers.size() != 0;
+    }
+
+    private boolean checkSelfMat() {
+        // On cherche la position de son roi
+        List<Coord> kingPos = findKings();
+        Coord allyKingPos = turn != PlayerColor.WHITE ? kingPos.get(1) : kingPos.get(0);
+
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (board[i][j] != null && board[i][j].color != turn) {
+                    // On récupère la liste des positions ou l'adversaire peut nous manger une pièce
+                    List<List<Coord>> moves = board[i][j].listEatingMove();
+                    List<List<Coord>> eatingMoves = refactorListMove(moves);
+
+                    // On regarde si une pièce peut manger notre roi
+                    if (findCoordInListMove(eatingMoves, allyKingPos.getX(), allyKingPos.getY())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private List<Coord> findKings() {   // list[0] = white, liste[1] = black
+        List<Coord> kingsPos = new LinkedList<Coord>();
+
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (board[i][j] != null && board[i][j].type == PieceType.KING) {
+                    if (board[i][j].color == PlayerColor.WHITE) {
+                        if (kingsPos.size() == 2) {
+                            kingsPos.add(0, board[i][j].coord);
+                            kingsPos.remove(1);
+                        } else {
+                            kingsPos.add(0, board[i][j].coord);
+                        }
+
+                    } else if (board[i][j].color == PlayerColor.BLACK && kingsPos.size() == 0) {
+                        Coord temp = new Coord(4, 0);
+                        kingsPos.add(0, temp);  // Position par défaut du roi blanc (pour éviter de trier la liste la fin et permet d'avoir toujours le roi blanc à l'index 0. Et permet d'éviter une exception)
+                        kingsPos.add(1, board[i][j].coord);
+                        continue;
+                    } else {
+                        kingsPos.add(1, board[i][j].coord);
+                    }
+                    if (kingsPos.size() == 2) {
+                        return kingsPos;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean canProtectHisKingByEating(LinkedList<Coord> attackers) {
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (board[i][j] != null && board[i][j].color != turn) {
+                    List<List<Coord>> allyMoves = board[i][j].listEatingMove();
+                    List<List<Coord>> defensiveMoveByEating = defensiveMoveByEating(allyMoves);
+
+                    for (Coord c : attackers) {
+                        if (findCoordInListMove(defensiveMoveByEating, c.getX(), c.getY())) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private List<List<Coord>> defensiveMoveByEating(List<List<Coord>> listMove) {
+        List<List<Coord>> refactorListMove = new LinkedList<>();
+        List<Coord> refactoredVect;
+        for (List<Coord> vect : listMove) {
+            refactoredVect = new LinkedList<>();
+            for (Coord c : vect) {
+                if (board[c.getX()][c.getY()] != null) {
+                    if (board[c.getX()][c.getY()].getColor() == turn) {
+                        refactoredVect.add(c);
+                        break;
+                    }
+                    if (vect.indexOf(c) < vect.size() && board[c.getX()][c.getY()].type != PieceType.KNIGHT) {
+                        break;
+                    }
+                }
+            }
+            if (!refactoredVect.isEmpty()) {
+                refactorListMove.add(refactoredVect);
+            }
+        }
+        return refactorListMove;
+    }
+
+    private boolean canCoverHisKing(LinkedList<Coord> attackers) {
+        // On cherche la position du roi allié qui est attaqué
+        List<Coord> kingPos = findKings();
+        Coord allyKingPos = turn == PlayerColor.WHITE ? kingPos.get(1) : kingPos.get(0);
+
+        List<Coord> listWhichContainsKingPos = new LinkedList<Coord>();
+
+        // On génére toute les positions des attaquants du roi
+        for (Coord c : attackers) {
+            List<List<Coord>> moves = board[c.getX()][c.getY()].listEatingMove();
+            List<List<Coord>> eatingMove = refactorListMove(moves);
+
+            // On cherche le vecteur qui va de la pièce attaquante au roi
+            for (List<Coord> l : eatingMove) {
+                for (Coord ck : l) {
+                    if (ck.isEqual(allyKingPos)) {
+                        listWhichContainsKingPos = l;
+                        listWhichContainsKingPos.remove(listWhichContainsKingPos.size() - 1);
+                        break;
+                    }
+                }
+                if (listWhichContainsKingPos.size() != 0) {
+                    break;
+                }
+            }
+
+            // Pour toute les pièces alliés, on regarde si une pièce peut s'interrposer entre le roi et l'attaquant
+            for (int i = 0; i < SIZE; ++i) {
+                for (int j = 0; j < SIZE; ++j) {
+                    if (board[i][j] != null && board[i][j].color != turn && board[i][j].type != PieceType.KING) {
+                        List<List<Coord>> allyMoves = board[i][j].listEatingMove();
+                        List<List<Coord>> defensiveMoveByMoving = defensiveMoveByCover(allyMoves); //mskn
+
+                        if (defensiveMoveByMoving.size() == 0) {
+                            continue;
+                        }
+
+                        for (Coord pos : listWhichContainsKingPos) {
+                            if (findCoordInListMove(defensiveMoveByMoving, pos.getX(), pos.getY())) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private List<List<Coord>> defensiveMoveByCover(List<List<Coord>> listMove) {
+        List<List<Coord>> refactorListMove = new LinkedList<>();
+        List<Coord> refactoredVect;
+        for (List<Coord> vect : listMove) {
+            refactoredVect = new LinkedList<>();
+            for (Coord c : vect) {
+                if (board[c.getX()][c.getY()] == null) {
+                    refactoredVect.add(c);
+                }
+                break;
+            }
+            if (!refactoredVect.isEmpty()) {
+                refactorListMove.add(refactoredVect);
+            }
+        }
+        return refactorListMove;
+    }
+
+    private boolean canMoveHisKing(LinkedList<Coord> attackers) {
+        // On cherche la position du roi allié qui est attaqué
+        List<Coord> kingPos = findKings();
+        Coord allyKingPos = turn == PlayerColor.WHITE ? kingPos.get(1) : kingPos.get(0);
+
+        // Génère les déplacements possibles du roi attaqué
+        List<List<Coord>> allyMoves = board[allyKingPos.getX()][allyKingPos.getY()].listEatingMove();
+        List<List<Coord>> defensiveMoveByMoving = defensiveMoveByCover(allyMoves);
+
+        List<Coord> listWhichContainsKingPos = new LinkedList<Coord>();
+
+        // On génére toute les positions des attaquants du roi
+        for (Coord c : attackers) {
+            List<List<Coord>> moves = board[c.getX()][c.getY()].listEatingMove();
+            List<List<Coord>> eatingMove = refactorListMove(moves);
+
+            // On cherche le vecteur qui va de la pièce attaquante au roi
+            for (List<Coord> l : eatingMove) {
+                for (Coord ck : l) {
+                    if (ck.isEqual(allyKingPos)) {
+                        listWhichContainsKingPos = l;
+                        listWhichContainsKingPos.remove(listWhichContainsKingPos.size() - 1);
+                        break;
+                    }
+                }
+                if (listWhichContainsKingPos.size() != 0) {
+                    break;
+                }
+            }
+
+            // On enlève les dépalcements du roi qui sont dans le vecteur d'attaque de l'attaquant
+            for (List<Coord> l : defensiveMoveByMoving) {
+                for (Coord kingMove : l) {
+                    for (Coord enemyMove : listWhichContainsKingPos) {
+                        if (enemyMove.isEqual(kingMove)) {
+                            l.remove(kingMove);
+
+                            if (l.size() == 0) {
+                                defensiveMoveByMoving.remove(l);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return defensiveMoveByMoving.size() > 0;
     }
 }
--- WSI_OBE_POO2022-L8-Chess/src/engine/util/MoveKnight.java
+++ WSI_OBE_POO2022-L8-Chess/src/engine/util/MoveKnight.java
@@ -20,44 +20,44 @@ public class MoveKnight extends Movement {
     @Override
     public List<List<Coord>> listMove(Coord coord) {
         List<List<Coord>> vectors = new LinkedList<>();
-        List<Coord> v = new LinkedList<>();
-        int coefX = 1,coefY = 2;
-        for(int i = 0; i < NBR_VECTORS; ++i){
-            v = new ArrayList<>();
-            for(int j = 1; j < this.range + 1; ++j){
-                try{
-                    v.add(new Coord(coord.getX() + j*coefX,coord.getY() + j*coefY));
-                }catch (RuntimeException e){
-                    System.out.println(e.getMessage());
-                    break;
-                }
+
+        int coefX = 1, coefY = 2;
+        for (int i = 0; i < NBR_VECTORS; ++i) {
+            List<Coord> v = new LinkedList<>();
+
+            try {
+                v.add(new Coord(coord.getX() + coefX, coord.getY() + coefY));
+                vectors.add(v);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
-            vectors.add(v);
-            if(i == 0) {
+
+
+            if (i == 0) {
                 coefX = 2;
                 coefY = 1;
             }
-            if(i == 1) {
+            if (i == 1) {
                 coefX = 2;
                 coefY = -1;
             }
-            if(i == 2) {
+            if (i == 2) {
                 coefX = 1;
                 coefY = -2;
             }
-            if(i == 3) {
+            if (i == 3) {
                 coefX = -1;
                 coefY = -2;
             }
-            if(i == 4) {
+            if (i == 4) {
                 coefX = -2;
                 coefY = -1;
             }
-            if(i == 5) {
+            if (i == 5) {
                 coefX = -2;
                 coefY = 1;
             }
-            if(i == 6) {
+            if (i == 6) {
                 coefX = -1;
                 coefY = 2;
             }
